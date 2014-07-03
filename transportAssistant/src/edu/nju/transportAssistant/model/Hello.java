4b84de4173141f3a7887718a3cd495dc5954ed6b
Hazel <cfzljcl@gmail.com>
2014-07-03 19:12:57 +0800

    JSF hello

---
+++ transportAssistant/src/edu/nju/transportAssistant/model/Hello.java
@@ -0,0 +1,12 @@
+package edu.nju.transportAssistant.model;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Hello {
+	final String world = "Hello World!";
+	
+	public String getworld() {
+		return world;
+	}
+}
